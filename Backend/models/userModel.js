9fad908bf7a36127fdf060cb5ef5b899fc763b8c
bhuvana shri <bhuvana.shri.s70@kalvium.community>
2025-04-17 12:19:27 +0530

    done schema

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,14 +1,11 @@
+// models/User.js
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: 
-  { 
-    type: String, 
-    unique: true 
-  },
-  password: String,
-  profilePicture: String,
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
