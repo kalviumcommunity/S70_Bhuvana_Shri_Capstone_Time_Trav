ff604e4f363a3fe48233b9bd8c879ac393ce94a3
bhuvana shri <bhuvana.shri.s70@kalvium.community>
2025-05-30 14:16:49 +0530

    Deleted unused route and test files

--- middleware/error.js
+++
@@ -1,8 +0,0 @@
-//middleware/error.js
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: err.message });
-  };
-  
-  module.exports = errorHandler;
-  
